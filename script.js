cdfb950c69f4efaa02c7ee010e814ef5c6483c8e
Ram Anuj <121351615+Er-Ram-Anuj@users.noreply.github.com>
2023-09-04 01:51:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let value=0;
+function Decf(){
+  value=value-1;
+  document.getElementById("Display").innerHTML=value;
+}
+function Incf(){
+  value=value+1;
+  document.getElementById("Display").innerHTML=value;
+}
+function Rstf(){
+  value=0;
+  document.getElementById("Display").innerHTML=value;
+}
